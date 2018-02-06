8477d5429036491e4ef8d1aa9a164b858321a09e
Matthew Renn <rennmatthewp@gmail.com>
2018-02-06 16:33:02 -0700

    writes test for async fetch in App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { App, mapDispatchToProps } from './App';
 import { shallow } from 'enzyme';
+import * as api from '../../helper/api';
 
 describe('App', () => {
   it('should match the snapshot', () => {
-    const renderedApp = shallow(<App />, {disableLifecycleMethods: true});
+    const renderedApp = shallow(<App />, { disableLifecycleMethods: true });
 
     expect(renderedApp).toMatchSnapshot();
-  })
-})
+  });
+
+  it('should call handleFetch in componentDidMount', async () => {
+    const mockFilms = [{}, {}, {}, {}, {}];
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () =>
+          Promise.resolve({
+            mockFilms
+          })
+      });
+    });
+    api.fetchApi = () => mockFilms;
+    const mockHandleFetch = jest.fn();
+    const renderedApp = await shallow(<App handleFetch={mockHandleFetch} />);
+
+    expect(mockHandleFetch).toHaveBeenCalled();
+  });
+});

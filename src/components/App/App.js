dfaa3adee80eb99e4c3dd39311eeaddadbb72418
Matthew Renn <rennmatthewp@gmail.com>
2018-02-06 13:00:24 -0700

    pulls fetchApi out of dispatch and calls it in CDM removes MSP

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,39 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { storeFilms } from '../../actions';
-
 import { Header } from '../Header/Header';
 import { Main } from '../Main/Main';
-import { fetchApi } from '../../helper/api'
+import { fetchApi } from '../../helper/api';
 import { url } from '../../helper/.api-key';
 import './App.css';
 
 export class App extends Component {
-  //add export for testing
-
-  componentDidMount() {
-    //call fetch api url and store in a variable
-    //pass fetch int this.props.handleFetch
-    this.props.handleFetch(url);
+  async componentDidMount() {
+    const filmsArray = await fetchApi(url);
+    this.props.handleFetch(filmsArray);
   }
 
   render() {
     return (
       <div className="App">
-       <Header />
-       <Main />
+        <Header />
+        <Main />
       </div>
     );
   }
 }
 
-export const mapStateToProps = (state) => ({
-  films: state.films
-})
+export const mapDispatchToProps = dispatch => ({
+  handleFetch: films => dispatch(storeFilms(films))
+});
 
-export const mapDispatchToProps = (dispatch) => ({
-  //don't do async/await 
-  handleFetch: async(url) => dispatch(storeFilms( await fetchApi(url)))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(null, mapDispatchToProps)(App);

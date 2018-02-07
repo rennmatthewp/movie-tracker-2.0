7ae3707a647497deaac69a3dcf3c6c95531987e6
Matthew Renn <rennmatthewp@gmail.com>
2018-02-06 21:03:06 -0700

    exports all pertinent consts for testing formatts for linter adds PropTypes

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Card } from '../Card/Card';
 import './CardContainer.css';
 
-const CardContainer = (props) => {
-  const filmCards = props.films.map( (film, index) => {
-      return (
-        <Card {...film} key={ index } />
-      )
-    })
+export const CardContainer = props => {
+  const filmCards = props.films.map((film, index) => (
+    <Card {...film} key={index} />
+  ));
 
   return (
-    <section className='CardContainer'>
-      { filmCards }
+    <section className="CardContainer">
+      {filmCards}
     </section>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state) => ({
+CardContainer.propTypes = {
+  films: PropTypes.arrayOf(PropTypes.object)
+};
+
+export const mapStateToProps = state => ({
   films: state.films
-})
+});
 
 export default connect(mapStateToProps, null)(CardContainer);

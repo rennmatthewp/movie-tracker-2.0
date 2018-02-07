304e01f1eaef33b4fb796685a086ef0fcde83116
Matthew Renn <rennmatthewp@gmail.com>
2018-02-06 20:59:15 -0700

    adds PropTypes and lints Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Card.css';
 
-export const Card = ({ poster, title, overview, date, rating, id}) => {
+export const Card = ({ poster, title, overview, date }) => {
   return (
-    <article className='Card'>
-      <img className='poster' src={`https://image.tmdb.org/t/p/w500${poster}`} alt="movie-poster" />
-      <div className='information'>
-        <h2>{ title }</h2>
-        <h3>{ date }</h3>
-        <p>{ overview }</p>
+    <article className="Card">
+      <img
+        className="poster"
+        src={`https://image.tmdb.org/t/p/w500${poster}`}
+        alt="movie-poster"
+      />
+      <div className="information">
+        <h2>{title}</h2>
+        <h3>{date}</h3>
+        <p>{overview}</p>
       </div>
     </article>
-  )
-}
+  );
+};
+
+Card.propTypes = {
+  poster: PropTypes.string,
+  title: PropTypes.string,
+  overview: PropTypes.string,
+  date: PropTypes.string,
+  rating: PropTypes.number,
+  id: PropTypes.number
+};

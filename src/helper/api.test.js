7d1df542ddcb904372da3fcd6d85184775a5589e
Casey Dallavalle <cbdallavalle@gmail.com>
2018-02-11 16:53:54 -0700

    Add api.test.js mockData into separate mockData file

--- src/helper/api.test.js
+++ src/helper/api.test.js
@@ -1,15 +1,10 @@
+/* eslint-disable */
 import * as api from './api';
 import { shallow } from 'enzyme';
+import { mockData } from '../mockData';
 
 describe('fetchAPI', () => {
   it('should call a fetch and return a resolved promise', () => {
-    const mockFilms = [      
-      { title: 'FBDO' },
-      { title: 'FBDO' },
-      { title: 'FBDO' },
-      { title: 'FBDO' },
-      { title: 'FBDO' }
-    ]
 
     window.fetch = jest.fn().mockImplementation(() => {
       return Promise.resolve({
@@ -17,7 +12,7 @@ describe('fetchAPI', () => {
         status: 200,
         json: () =>
           Promise.resolve({
-            results: mockFilms
+            results: mockData.mockFilmsArray
           })
       });
 
@@ -41,30 +36,6 @@ describe('fetchAPI', () => {
 
 describe('getFilms', () => {
   it('should call a fetch and return a cleaned film object', async() => {
-    const mockFilms =     
-    [
-      {
-        "vote_count": 6793,
-        "id": 198663,
-        "video": false,
-        "vote_average": 7,
-        "title": "The Maze Runner",
-        "popularity": 535.445142,
-        "poster_path": "/coss7RgL0NH6g4fC2s5atvf3dFO.jpg",
-        "original_language": "en",
-        "original_title": "The Maze Runner",
-        "genre_ids": [
-        28,
-        9648,
-        878,
-        53
-        ],
-        "backdrop_path": "/lkOZcsXcOLZYeJ2YxJd3vSldvU4.jpg",
-        "adult": false,
-        "overview": "Set in a post-apocalyptic world, young Thomas is deposited in a community of boys after his memory is erased, soon learning they're all trapped in a maze that will require him to join forces with fellow “runners” for a shot at escape.",
-        "release_date": "2014-09-10"
-      }
-    ]
 
     window.fetch = jest.fn().mockImplementation(() => {
       return Promise.resolve({
@@ -72,7 +43,7 @@ describe('getFilms', () => {
         status: 200,
         json: () =>
           Promise.resolve({
-            results: mockFilms
+            results: [mockData.mockRawFilmData]
           })
       });
     });
@@ -83,43 +54,7 @@ describe('getFilms', () => {
 
 describe('cleanFilms', () => {
   it('should clean a films array', () => {
-
-  const mockFilms =     
-    [
-      {
-        "vote_count": 6793,
-        "id": 198663,
-        "video": false,
-        "vote_average": 7,
-        "title": "The Maze Runner",
-        "popularity": 535.445142,
-        "poster_path": "/coss7RgL0NH6g4fC2s5atvf3dFO.jpg",
-        "original_language": "en",
-        "original_title": "The Maze Runner",
-        "genre_ids": [
-        28,
-        9648,
-        878,
-        53
-        ],
-        "backdrop_path": "/lkOZcsXcOLZYeJ2YxJd3vSldvU4.jpg",
-        "adult": false,
-        "overview": "Set in a post-apocalyptic world, young Thomas is deposited in a community of boys after his memory is erased, soon learning they're all trapped in a maze that will require him to join forces with fellow “runners” for a shot at escape.",
-        "release_date": "2014-09-10"
-      }
-    ]
-  const cleanedFilms = [
-    {
-      backdrop: "/lkOZcsXcOLZYeJ2YxJd3vSldvU4.jpg",
-      title: "The Maze Runner",
-      overview: "Set in a post-apocalyptic world, young Thomas is deposited in a community of boys after his memory is erased, soon learning they're all trapped in a maze that will require him to join forces with fellow “runners” for a shot at escape.",
-      poster_path: "/coss7RgL0NH6g4fC2s5atvf3dFO.jpg",
-      release_date: "2014-09-10",
-      vote_average: 7,
-      movie_id: 198663
-    }
-  ]
-  expect(api.cleanFilms(mockFilms)).toEqual(cleanedFilms);
+  expect(api.cleanFilms([mockData.mockRawFilmData])).toEqual([mockData.mockCleanedFilmData]);
   })
 })
 
@@ -130,16 +65,7 @@ describe('getUserData', () => {
       status: 200,
       json: () =>
         Promise.resolve({
-          data: [
-            { 
-              user: {
-                id: 12,
-                name: 'Matt',
-                email: '123@gmail.com',
-                password: 'password' 
-              }
-            }
-          ]
+          data: [{ user: mockData.mockUser }]
         })
     });
   });
@@ -173,46 +99,26 @@ describe('sendFavorite', () => {
 describe('deleteFavorite', () => {
 
   it('should call deleteFavorite with the correct params', () => {
-    const mockUser = {
-                id: 12
-              }
-    const mockFilm = {
-      movie_id: 17
-    }
-    const mockFilmToDelete = {
-      id: 12,
-      filmId: 17
-    }
-
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       json: () => Promise.resolve({
         response: {}
       })
     }))
 
-     const expectParams = [
+    const expectParams = [
       'http://localhost:3000/api/users/12/favorites/17',
       {
         method: 'DELETE',
-        body: JSON.stringify(mockFilmToDelete),
+        body: JSON.stringify({ id: 12, filmId: 17 }),
       }
     ]
 
-    api.deleteFavorite(mockUser, mockFilm);
+    api.deleteFavorite({ id: 12 } , { movie_id: 17 });
     expect(window.fetch).toHaveBeenCalledWith(...expectParams);
   })
 })
 
 describe('postFetch', () => {
-  const mockFavoriteToStore = {
-    movie_id: 2,
-    user_id: 10,
-    title: '',
-    poster_path: '',
-    release_date: '',
-    vote_average: '',
-    overview: ''
-  }
 
   it('should call with the post fetch with the correct params', () => {
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
@@ -224,12 +130,12 @@ describe('postFetch', () => {
     'http://localhost:3000/api/users/favorites/new',
     {
       method: 'POST',
-      body: JSON.stringify(mockFavoriteToStore),
+      body: JSON.stringify(mockData.mockFavoriteToStore),
       "headers": {"map": {"content-type": "application/json"}}
     }
   ]
 
-  api.postFetch('users/favorites/new', mockFavoriteToStore, 'POST' );
+  api.postFetch('users/favorites/new', mockData.mockFavoriteToStore, 'POST' );
   expect(window.fetch).toHaveBeenCalledWith(...expectParams);
     
   })
@@ -241,7 +147,7 @@ describe('postFetch', () => {
         status: 400,
       })
     })
-    const response = await api.postFetch('url', mockFavoriteToStore, 'POST')
+    const response = await api.postFetch('url', mockData.mockFavoriteToStore, 'POST')
     expect(response).toEqual(Error('Bad response stats'))
     expect(window.fetch).toHaveBeenCalled()
   });
--- src/mockData.js
+++ src/mockData.js
@@ -72,5 +72,44 @@ export const mockData = {
     name: "", 
     password: ""
   },
+  mockRawFilmData: {
+    "vote_count": 6793,
+    "id": 198663,
+    "video": false,
+    "vote_average": 7,
+    "title": "The Maze Runner",
+    "popularity": 535.445142,
+    "poster_path": "/coss7RgL0NH6g4fC2s5atvf3dFO.jpg",
+    "original_language": "en",
+    "original_title": "The Maze Runner",
+    "genre_ids": [
+    28,
+    9648,
+    878,
+    53
+    ],
+    "backdrop_path": "/lkOZcsXcOLZYeJ2YxJd3vSldvU4.jpg",
+    "adult": false,
+    "overview": "Set in a post-apocalyptic world, young Thomas is deposited in a community of boys after his memory is erased, soon learning they're all trapped in a maze that will require him to join forces with fellow “runners” for a shot at escape.",
+    "release_date": "2014-09-10"
+  },
+  mockCleanedFilmData: {
+    backdrop: "/lkOZcsXcOLZYeJ2YxJd3vSldvU4.jpg",
+    title: "The Maze Runner",
+    overview: "Set in a post-apocalyptic world, young Thomas is deposited in a community of boys after his memory is erased, soon learning they're all trapped in a maze that will require him to join forces with fellow “runners” for a shot at escape.",
+    poster_path: "/coss7RgL0NH6g4fC2s5atvf3dFO.jpg",
+    release_date: "2014-09-10",
+    vote_average: 7,
+    movie_id: 198663
+  },
+  mockFavoriteToStore: {
+    movie_id: 2,
+    user_id: 10,
+    title: '',
+    poster_path: '',
+    release_date: '',
+    vote_average: '',
+    overview: ''
+  }
 }
 

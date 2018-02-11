b963e54750170f8e0256e8e5b3b731ee2ffa8351
Casey Dallavalle <cbdallavalle@gmail.com>
2018-02-11 13:55:44 -0700

    Sign Up Tests Add testing to make sure that handleSubmit calls it's login prop. When an error in the fetch is called, state updates and the HTML updates

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Login } from './Login';
+import * as api from '../../helper/api';
 
 //in addition to the two tests written but not finished:
 //test MDTP
@@ -13,6 +14,18 @@ describe('Login', () => {
     const renderedLogin = shallow(<Login />);
 
     expect(renderedLogin).toMatchSnapshot();
+  });
+
+  it('should call handleSubmit on submit of the form', async () => {
+    const mockLogin = jest.fn();
+    const renderedLogin = shallow(<Login logIn={mockLogin}/>)
+    const mockUser = {name: 'Jordan', email: 'email', password: 'password'}
+
+    api.getUserData = (url, state) => mockUser;
+    const mockEvent = { preventDefault: () => {} }
+    await renderedLogin.instance().handleSubmit(mockEvent);
+    expect(api.getUserData()).toEqual({"email": "email", "name": "Jordan", "password": "password"});
+    expect(mockLogin).toHaveBeenCalled();
   });
 
   xit('should call handleInputChange onChange of the inputs', () => {});

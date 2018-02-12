de7821bf7de6a544198cb1fd4ef9ef32e1f2ded8
Matthew Renn <rennmatthewp@gmail.com>
2018-02-12 09:17:22 -0700

    Delete .api-key.js

--- src/helper/.api-key.js
+++
@@ -1,3 +0,0 @@
-export const key = '030166c9615690cd48d154e971eb528a'
-
-export const url = 'https://api.themoviedb.org/3/discover/movie?api_key=030166c9615690cd48d154e971eb528a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
